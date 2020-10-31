4ec41a84329bf7ec690e7847b4d12e2fd714c44e
semsol23 <70558356+semsol23@users.noreply.github.com>
2020-10-31 19:53:00 +0900

    2-2

--- main.c
+++ main.c
@@ -5,15 +5,19 @@
 
 int main(int argc, char *argv[]) {
 	
-	int i;
+	int i, average;
+	int sum=0;
 	int grade[SIZE];
-	printf("5명의 점수를 입력하세요. \n");
 	
-	for (i=0; i<SIZE; i++)
+	for(i=0; i<SIZE; i++)
+	{
+		printf("학생 성적을 입력하세요 : ");
 		scanf("%d", &grade[i]);
-		
-	for (i=0; i<SIZE; i++)
-		printf("grade[%d] = %d\n", i, grade[i]);
+		sum += grade[i];
+	}
+
+	average = sum/SIZE;
+	printf("성적 평균 : %d\n", average);
 
 	return 0;
 }
